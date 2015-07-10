b950b1393fb7f5ad034ccc2794b260f189ee1e30
Adegeye Mayowa <mayox4ever2006@gmail.com>
2015-07-10 11:28:52 +0100

    myprojectwork

---
+++ mj.js
@@ -0,0 +1,16 @@
+$(document).ready( function () {
+  $('#button_click').click(function () {  
+    var form_value = $("#USERNAME").val();
+    console.log(form_value);
+    $.ajax({
+    url: "https://api.github.com/users/" + form_value ,
+    dataType: 'json',
+    success: function(results){
+      console.log(results);
+        var url_go = "https://github.com/" + form_value;
+
+      //window.location.href= url_go;
+  }
+  }); 
+});
+});
